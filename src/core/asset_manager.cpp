ac67c7e00f22fe2719d98feb1bbfa0c9e76ca49d
BrunoSXS <trustmeiamadeveloper@gmail.com>
2021-06-08 21:10:27 -0300

    added simple asset manager

---
+++ src/core/asset_manager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by brunosxs on 08/06/2021.
+//
+
+#include "asset_manager.h"
---
+++ src/core/asset_manager.h
@@ -0,0 +1,14 @@
+//
+// Created by brunosxs on 08/06/2021.
+//
+
+#ifndef RETRODL8_ASSET_MANAGER_H
+#define RETRODL8_ASSET_MANAGER_H
+
+
+class asset_manager {
+
+};
+
+
+#endif //RETRODL8_ASSET_MANAGER_H
