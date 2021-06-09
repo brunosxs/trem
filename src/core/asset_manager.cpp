59e7e4370873d3acb0c61cfc5509038319e74b56
BrunoSXS <trustmeiamadeveloper.com>
2021-06-08 21:11:05 -0300

    added map for component types

--- src/core/asset_manager.cpp
+++ src/core/asset_manager.cpp
@@ -3,3 +3,20 @@
 //
 
 #include "asset_manager.h"
+#include "texture_manager.h"
+
+AssetManager::AssetManager(EntityManager *manager): manager(manager) {
+
+}
+
+void AssetManager::ClearData() {
+    textures.clear();
+}
+
+void AssetManager::AddTexture(std::string textureId, const char *filePath) {
+    textures.emplace(textureId, TextureManager::LoadTexture(filePath));
+}
+
+SDL_Texture* AssetManager::GetTexture(std::string textureId) {
+    return textures[textureId];
+}
